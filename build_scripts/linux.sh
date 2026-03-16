99b8fc0576361b7c1ecb56a67261f552ea4c8bca
hw-erl-lnaylor <luke.naylor@h-partners.com>
2026-03-16 11:41:41 +0000

    WIP untested: containerized build trim and optimisation

--- build_scripts/linux.sh
+++ build_scripts/linux.sh
@@ -18,6 +18,8 @@ echo $WORKSPACE
 #
 cd $WORKSPACE/cangjie_compiler;
 [ "$SKIP_CLEAN" -eq 1 ] || python3 build.py clean;
+[ -d third_party/llvm-project ] || git clone ../third_party/llvm-project/ third_party/llvm-project
+[ -d third_party/flatbuffers ] || git clone ../third_party/third_party_flatbuffers third_party/flatbuffers
 python3 build.py build -t release --no-tests;
 python3 build.py install;
 
